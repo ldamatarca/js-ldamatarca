9eae34025bc0426be6d1907f912942fb64f08ae9
ldamatarca <leonard.damatarca@gmail.com>
2022-04-06 19:28:32 +0300

    adaugare cod JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 let x = 2 + 3 * 4 - 5;
-
+console.log(x);
 let y = 4 * (2 + 6 / 3);
-
+console.log(y);
 let z = 2 * 3 ** 2 + 2 ** 5;
-
+console.log(z);
 let w = 5 * (3 + 2 / 2) + 3;
+console.log(w);
