2bd91dfcedca9929b317833b64b33fdff67285ad
ldamatarca <leonard.damatarca@gmail.com>
2022-04-09 11:27:55 +0300

    Added script.js file2

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,69 @@
-let x = 2 + 3 * 4 - 5;
-console.log(x);
-let y = 4 * (2 + 6 / 3);
-console.log(y);
-let z = 2 * 3 ** 2 + 2 ** 5;
-console.log(z);
-let w = 5 * (3 + 2 / 2) + 3;
-console.log(w);
+const grade = 10;
+const limbaj = "HTML";
+
+// if (grade == 10) {
+//   console.log("Am nota 10!");
+// } else if (grade == 9) {
+//   console.log("Am nota 9!");
+// } else {
+//   console.log("Nu am nota 10!");
+// }
+
+// switch (grade) {
+//   case 10: //if (grade === "10")
+//     console.log("Am nota 10!");
+//     break;
+
+//   case 9: //if (grade === "9" || grade === "8.8")
+//   case 8.8:
+//     console.log("Am nota 9!");
+//     break;
+
+//   default:
+//     console.log("Nu am nota 10!");
+//     break;
+// }
+
+// const jsCourse = "WEB ITSchool";
+
+// const isItACourse = jsCourse ? "yes" : "no"
+
+// let isWinter = true;
+// let currentMonth = "July";
+
+// if (isWinter == true) {
+//   currentMonth = "Decemeber";
+// } else {
+//   currentMonth = "July";
+// }
+
+// currentMonth = isWinter === true ? "December" : "July";
+
+// currentMonth =
+//   isWinter === true ? "December" : isWinter === fasle ? "June" : "July";
+
+// let x = 2 + 3 * 4 - 5;
+// console.log(x);
+// let y = 4 * (2 + 6 / 3);
+// console.log(y);
+// let z = 2 * 3 ** 2 + 2 ** 5;
+// console.log(z);
+// let w = 5 * (3 + 2 / 2) + 3;
+// console.log(w);
+
+//Instructiuni repetitive WHILE//
+
+// let myNumber = 1;
+
+// while (myNumber < 100000) {
+//   console.log(myNumber);
+//   myNumber = myNumber + 1;
+// }
+
+for (let myNumber = 1; myNumber <= 10; myNumber++) {
+  console.log(myNumber);
+}
+
+//Instructiuni repetitive FOR//
+
+.
